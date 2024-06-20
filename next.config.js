406b866fa18819245408ceb971645c6943524eb5
JasonDL-trajector <emmanuel.delara@trajector.com>
2024-06-21 01:57:54 +0800

    fixed basePath

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// /** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/next-portfolio",
+    output: "export",
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
